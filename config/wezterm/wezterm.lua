f35e5cfc6d35060d5df1145c68b5f7c508481484
Bhargav Mogra <bhargav521@gmail.com>
2026-09-20 15:24:25 +0700

    feat: Use rose pine moon for dark mode

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,16 +2,16 @@ local wezterm = require 'wezterm'
 
 -- wezterm.gui is not available to the mux server, so take care to
 -- do something reasonable when this config is evaluated by the mux
-function get_appearance()
+local function get_appearance()
   if wezterm.gui then
     return wezterm.gui.get_appearance()
   end
   return 'Dark'
 end
 
-function scheme_for_appearance(appearance)
+local function scheme_for_appearance(appearance)
   if appearance:find 'Dark' then
-    return 'CGA'
+    return 'rose-pine-moon'
   else
     return 'GruvboxLight'
   end
